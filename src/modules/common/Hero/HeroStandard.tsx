b525901b4b22da4a4ad5f807c4a605ee7ccbde55
Harley Salas <harley@harleysalas.com>
2025-01-06 11:30:57 +0300

    fix page margin

--- src/modules/common/Hero/HeroStandard.tsx
+++ src/modules/common/Hero/HeroStandard.tsx
@@ -5,7 +5,7 @@ import { RichText } from '../RichText'
 export const HeroStandard = (props: Page['hero']) => {
   const { richText } = props
   return (
-    <div className="mt-header container pt-12 sm:pt-20 md:pt-24">
+    <div className="container mt-header pt-20 lg:pt-[9.25rem]">
       <RichText
         data={richText}
         prose="standard"
