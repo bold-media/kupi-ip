c1a5094dae0851acac4644de87e21fdf01945664
Harley Salas <harley@harleysalas.com>
2025-01-06 05:20:12 +0300

    feature block content

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20250105_134632 from './20250105_134632';
+import * as migration_20250106_021956 from './20250106_021956';
 
 export const migrations = [
   {
     up: migration_20250105_134632.up,
     down: migration_20250105_134632.down,
-    name: '20250105_134632'
+    name: '20250105_134632',
+  },
+  {
+    up: migration_20250106_021956.up,
+    down: migration_20250106_021956.down,
+    name: '20250106_021956'
   },
 ];
