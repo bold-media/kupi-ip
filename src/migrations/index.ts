88e54e5adadcd33a7f46ce83d30eea2ed9d05c23
Harley Salas <harley@harleysalas.com>
2025-01-06 18:57:22 +0300

    migrations for noIndex and Content Block

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20250105_134632 from './20250105_134632';
 import * as migration_20250106_021956 from './20250106_021956';
+import * as migration_20250106_155641 from './20250106_155641';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20250106_021956.up,
     down: migration_20250106_021956.down,
-    name: '20250106_021956'
+    name: '20250106_021956',
+  },
+  {
+    up: migration_20250106_155641.up,
+    down: migration_20250106_155641.down,
+    name: '20250106_155641'
   },
 ];
