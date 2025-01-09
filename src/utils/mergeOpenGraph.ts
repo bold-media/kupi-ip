306108922c631f6bce2c43fd45fb4d0c1fde5cf3
Harley Salas <harley@harleysalas.com>
2025-01-09 15:03:09 +0300

    update default og

--- src/utils/mergeOpenGraph.ts
+++ src/utils/mergeOpenGraph.ts
@@ -2,22 +2,22 @@ import type { Metadata } from 'next'
 // import { getServerSideURL } from './getURL'
 
 const defaultOpenGraph: Metadata['openGraph'] = {
-    type: "website",
-    description: 'Website built with NextJS and PayloadCMS.',
-    //no default og image
-    // images: [
-    //     {
-    //         url: `${getServerSideURL()}/website-default-OG.webp`
-    //     }
-    // ]
-    siteName: process.env.NEXT_PUBLIC_APP_NAME || "NextJS Website by Boldmedia",
-    title: process.env.NEXT_PUBLIC_APP_NAME || "NextJS Website by Boldmedia"
+  type: 'website',
+  description: 'Website built with NextJS and PayloadCMS.',
+  //no default og image
+  // images: [
+  //     {
+  //         url: `${getServerSideURL()}/website-default-OG.webp`
+  //     }
+  // ]
+  siteName: process.env.NEXT_PUBLIC_APP_NAME || 'Kupi IP',
+  title: process.env.NEXT_PUBLIC_APP_NAME || 'Kupi IP',
 }
 
 export const mergeOpenGraph = (og?: Metadata['openGraph']): Metadata['openGraph'] => {
-    return {
-        ...defaultOpenGraph,
-        ...og,
-        images: og?.images ? og.images : defaultOpenGraph?.images ? defaultOpenGraph.images : undefined
-    }
+  return {
+    ...defaultOpenGraph,
+    ...og,
+    images: og?.images ? og.images : defaultOpenGraph?.images ? defaultOpenGraph.images : undefined,
+  }
 }
